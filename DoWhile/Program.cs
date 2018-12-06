a9b2f851334cde3f342c0756b43b9a8df81cae9b
Glen Buck <glenbuck503@gmail.com>
2018-12-06 15:10:51 -0800

    added Do While and While exercise

---
+++ DoWhile/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try and guess my favorite Number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            bool guessed = userNum == 4;
+
+            do
+            {
+                switch (userNum)
+                {
+                    case 10:
+                        Console.WriteLine("Nope! Try again!");
+                        Console.WriteLine("Guess a different number");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 20:
+                        Console.WriteLine("Nope! Try again!");
+                        Console.WriteLine("Guess a different number");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 30:
+                        Console.WriteLine("Nope! Try again!");
+                        Console.WriteLine("Guess a different number");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 40:
+                        Console.WriteLine("Nope! Try again!");
+                        Console.WriteLine("Guess a different number");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("AhHah! Correct! Nice Guess!");
+                        guessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope! Try again!");
+                        Console.WriteLine("Guess a different number");
+                        userNum = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+            while (!guessed);
+            Console.Read();
+            
+            
+        }
+    }
+}
---
+++ While/While/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you think my name is?");
+            string name =(Console.ReadLine());
+            bool guessed = false;
+           
+            while (!guessed)
+            {
+                switch (name)
+                {
+                    case "Tim":
+                        Console.WriteLine("Sorry, that is not my name!");
+                        Console.WriteLine("What do you think my name is?");
+                        name = (Console.ReadLine());
+                        break;
+                    case "Ben":
+                        Console.WriteLine("Sorry, that is not my name");
+                        Console.WriteLine("What do you think my name is?");
+                        name = (Console.ReadLine());
+                        break;
+                    case "Jordan":
+                        Console.WriteLine("Sorry,that is not my name");
+                        Console.WriteLine("What do you think my name is?");
+                        name = (Console.ReadLine());
+                        break;
+                    case "Glen":
+                        Console.WriteLine("THAT IS MY NAME!");
+                        guessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Not even close!");
+                        Console.WriteLine("What do you think my name is?");
+                        name = (Console.ReadLine());
+                        break;
+
+                }
+
+            }
+            
+            Console.Read();
+
+            
+        }
+    }
+}
